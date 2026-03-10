fbab12c499c3fc1dcca38c3d35106b6db66f7573
Arash Soltani Hemmat <157841868+ArashSoltaniHemmat@users.noreply.github.com>
2026-03-10 15:33:55 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import ResultCard from './ResultCart.jsx'
+
+function App() {
+
+
+  return (
+    <div>
+      <ResultCard />
+    </div>
+  )
+}
+
+export default App
